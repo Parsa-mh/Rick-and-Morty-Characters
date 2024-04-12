7b0fc1c3981040528c7d42a016b714fecb77d536
Parsa-mh <mahmoodiparsa654@gmail.com>
2024-04-12 22:38:41 +0330

    added main page link to error component

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,5 +1,6 @@
 import image from "../../assets/img/error.png"
 import styles from "../../assets/styles/error.module.css"
+import { Link } from "react-router-dom"
 const Error = () => {
     return (
         <section className={styles.body} style={{height:"100vh"}}>
@@ -7,8 +8,11 @@ const Error = () => {
                 <div className="w-75 d-flex align-items-center justify-content-center mx-auto" style={{borderRadius : "18px 18px 0 0",overflow : "hidden"}}>
                     <img src={image} className="w-100" alt="404" />
                 </div>
-                <div className="text-light w-75 mx-auto d-flex align-items-center justify-content-center" style={{height : 70,borderRadius : "0 0 18px 18px",backgroundColor : "rgb(31, 4, 120)"}}>
+                <div className="text-light w-75 mx-auto d-flex flex-column align-items-center justify-content-center" style={{height : 90,borderRadius : "0 0 18px 18px",backgroundColor : "rgb(31, 4, 120)"}}>
                     <h2 className="text-center fs-1">Page Not Found</h2>
+                </div>
+                <div className="d-block text-center">
+                    <Link to={"/"} className="text-light fs-5">main page</Link>
                 </div>
             </div>
         </section>
